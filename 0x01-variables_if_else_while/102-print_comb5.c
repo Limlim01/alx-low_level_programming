32189cbdb8cd3af6820d606ed610ef2353fc9de4
salim <salimdauda2001@gmail.com>
2022-10-14 22:15:17 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/** 
+/**
  * main-entry point for the program
  * Return:0 success, non zero fail
  */
-int main (void)
+int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 99; i++)
 	{
 	for (j = i + 1; j <= 99; j++)
